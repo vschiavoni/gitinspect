01ecea6f6ca1d1752ce9717bee4e745cf7558855
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-10-24 19:23:58 +0200

    get the size of a file at any given revision of the repository

--- inspect.sh
+++ inspect.sh
@@ -5,7 +5,8 @@ cd $path
 filename=$2
 for sha in $(git log --pretty=format:%h); do 
 	git checkout $sha; 
-	echo $sha $(wc -l $filename) >> /tmp/file-history.txt; 
+	echo $sha $(wc -c $filename) >> /tmp/file-history.txt; 
 done
+echo "Back to master branch for repository at $path"
 git checkout master
 #git log --abbrev-commit --pretty=oneline --merges | cut -f 1 -d" " | xargs git checkout
