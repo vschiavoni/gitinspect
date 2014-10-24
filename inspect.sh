1858e406c3fe4a2767ee82413fc71ccd8d51bfa0
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-10-24 19:03:09 +0200

    get arg from cmdline

--- inspect.sh
+++ inspect.sh
@@ -2,7 +2,7 @@
 path=$1
 cd $path
 #todo check that it is a GIT repository
-
+filename=$2
 for sha in $(git log --pretty=format:%h); do 
 	git checkout $sha; 
 	echo $sha $(wc -l $filename) >> /tmp/file-history.txt; 
